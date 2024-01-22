3ef1c00bfe35b44137125b144d305dded022c74d
Hohm <Hohm@HOHM>
2024-01-22 16:16:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi " + name );
+            Console.WriteLine("How old are you");
+            string age = Console.ReadLine(); ;
+            Console.WriteLine("You are " + age);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
